6e3e67cf2ef464d807d92dfa4eb502d40668adc6
¨Franco <frangarcia93@icloud.com>
2022-08-02 21:34:06 -0300

    Primer React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import './assets/css/app.css';
+
+import SlideBar from './components/SlideBar';
+import ContentWrapper from './components/ContentWrapper'
+
+function App() {
+  return (
+<div id = "wrapper" >
+    <SlideBar />
+    <ContentWrapper />
+</div>
+
+  );
+}
+
+export default App;
